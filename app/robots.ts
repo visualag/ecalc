836448bd8764c3669349a3f824c2e8e23edf6fd8
TheniusFogas <152709030+TheniusFogas@users.noreply.github.com>
2026-02-08 22:58:09 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin-pro/'],
+      disallow: '/admin-pro/',
     },
     sitemap: 'https://ecalc.ro/sitemap.xml',
   }
